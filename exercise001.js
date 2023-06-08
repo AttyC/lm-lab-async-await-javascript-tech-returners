604e883d1a85df7260152623cdfc8876bc118088
Atty Cronin <antoniajcronin@gmail.com>
2023-06-08 12:01:23 +0100

    Refactor to use async/await

--- exercise001.js
+++ exercise001.js
@@ -1,18 +1,17 @@
-const promise = new Promise((resolve, reject) => {
-  // Generate random number between 0 and 9
+const getResponse = async () => {
   const randomInt = Math.floor(Math.random() * 10);
-
-  if (randomInt % 2 === 0) {
-    // Success
-    setTimeout(() => resolve("done"), 2000);
-  } else {
-    // Failure
-    setTimeout(() => reject("error"), 2000);
+  try {
+    randomInt % 2 === 0
+      ? setTimeout(
+          () => console.log(`Yay! Promise resolved with response: done`),
+          2000
+        )
+      : setTimeout(
+          () => console.log(`Boo. Promise rejected with response: error`),
+          2000
+        );
+  } catch {
+    (e) => console.log(e);
   }
-});
-
-promise
-  .then((response) =>
-    console.log(`Yay! Promise resolved with response: ${response}`)
-  )
-  .catch((e) => console.log(`Boo. Promise rejected with response: ${e}`));
+};
+getResponse();
